d8ffd893805ee4eee0ef8babcdef5cc06b3f8b04
ocarrillor <ocarrillo7@ucol.mx>
2020-12-09 16:03:20 -0600

    Implemented function factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+    factorial(numero){
+        let fact = numero;
+        for(let i = numero; i > 1; i--){
+            fact = fact * (i-1);
+        }
+        return fact;
+    }
+}
 
 let app = new App();
+
+//Demostracion de la funcion factorial.
+console.log(app.factorial(5));
